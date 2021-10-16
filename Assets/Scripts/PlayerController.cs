a37d24193f7ad4d72ab9feeabad572e1413bbd23
Justin <justinmorettomtl@gmail.com>
2021-10-15 21:57:35 -0400

    updated movement controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    [SerializeField]private float movementSpeed = 20;
-    [SerializeField]private float gravity = -5;
-    private CharacterController myController = null;
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+    [SerializeField]
+    private float gravity = -5;
     private Vector3 moveDirection = Vector3.zero;
+    //ROTATE VARIABLE
+    public float rotationSpeed = 90;
+    private Vector3 rotationDirection = Vector3.zero;
+    //OTHER STUFF
+    private CharacterController myController = null;
+
+
+    // Start is called before the first frame update
     void Awake()
     {
-        Debug.Log(myController);
         myController = GetComponent<CharacterController>();
-        Debug.Log(myController);
     }
+
+    // Update is called once per frame
     void Update()
     {
-         
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-        {
-            moveDirection = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal") * movementSpeed, gravity, Input.GetAxis("Vertical") * movementSpeed));
-            myController.Move(moveDirection * Time.deltaTime);
-        }
+        //MOVE
+        moveDirection = transform.TransformDirection(new Vector3(0, gravity, Input.GetAxis("Vertical") * moveSpeed));
+        myController.Move(moveDirection * Time.deltaTime);
+        //ROTATE
+        rotationDirection = new Vector3(0, Input.GetAxis("Horizontal") * rotationSpeed, 0);
+        transform.Rotate(rotationDirection * Time.deltaTime, Space.Self);
+        //JUMP
+
     }
+    //void Update()
+    //{
+         
+    //    if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+    //    {
+    //        moveDirection = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal") * movementSpeed, gravity, Input.GetAxis("Vertical") * movementSpeed));
+    //        myController.Move(moveDirection * Time.deltaTime);
+    //    }
+    //}
 }
