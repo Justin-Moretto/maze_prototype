eff1ff21283fe3bfc30f6c99a2a34f05bda20ff4
Bryan Internoscia <inkrocker@outlook.com>
2021-10-19 10:20:51 -0400

    Maze entrance doors animated + scripted + floor tiles + mats & txrs updates

---
+++ Assets/Scripts/TriggerZone_M2Doors.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone_M2Doors : MonoBehaviour
+{
+    public GameObject doorL_Target = null;
+    public GameObject doorR_Target = null;
+
+    private void OnTriggerEnter(Collider playerAvatar)
+    {
+        if(playerAvatar.gameObject.tag == "Player")
+        {
+            doorL_Target.SendMessage("DoorsAnimation");
+            doorR_Target.SendMessage("DoorsAnimation");
+        }
+    }
+}
