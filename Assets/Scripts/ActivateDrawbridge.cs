2ca40ffdc23d02f7af8a1fef10a66d462f46cd82
Bryan Internoscia <inkrocker@outlook.com>
2021-10-21 17:21:05 -0400

    started scripting lever

--- Assets/Scripts/ActivateDrawbridge.cs
+++ Assets/Scripts/ActivateDrawbridge.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class ActivateDrawbridge : MonoBehaviour
 {
     public GameObject drawbridge;
+    public GameObject lever;
+
+    public void Awake()
+    {
+        lever.GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         drawbridge.SetActive(true);
         //activate drawbridge animation here
     }
+
+    /*public void ActivateLever()
+    {
+        lever.SetBool("LeverCanPlay");
+    }*/
 }
