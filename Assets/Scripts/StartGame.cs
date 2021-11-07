b19481d12bb10ac3b7908eba67416c4f6d1e04aa
Justin <justinmorettomtl@gmail.com>
2021-11-06 23:26:43 -0400

    TItlescreen -> MainScene script

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainScene");
+        }
+    }
+}
---
+++ Assets/Scripts/TextFlicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFlicker : MonoBehaviour
+{
+    Text text;
+    float a = 255;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //a -= Time.deltaTime;
+        //text.color = new Color(255, 165, 0, a);
+    }
+}
