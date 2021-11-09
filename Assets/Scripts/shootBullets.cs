45f3f88a23045648593f6c1b9bf122c8b1fa6ba1
Justin <justinmorettomtl@gmail.com>
2021-11-08 19:47:06 -0500

    minor tweaks

--- Assets/Scripts/shootBullets.cs
+++ Assets/Scripts/shootBullets.cs
@@ -23,7 +23,7 @@ public class shootBullets : MonoBehaviour
     void Update()
     {
         timer = timer + Time.deltaTime;
-        shotInterval = 500 / firingSpeed; //delete this line for final build. Only here for testing. Only needs to be called in Awake();
+        //shotInterval = 500 / firingSpeed; //delete this line for final build. Only here for testing. Only needs to be called in Awake();
 
         if (timer >= shotInterval)
         {
