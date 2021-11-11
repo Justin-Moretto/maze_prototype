445e666689c0cd6d5c84e930b697807b182c1710
Justin <justinmorettomtl@gmail.com>
2021-11-10 20:33:07 -0500

    added treasure chest animation

---
+++ Assets/Scripts/AnimateChest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateChest : MonoBehaviour
+{
+    public GameObject chest;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        chest.GetComponent<Animator>().SetBool("Open", true);
+    }
+}
